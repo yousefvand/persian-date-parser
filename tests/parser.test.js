9babcc00be96e9d3786e5d2c43f0c01f709e602c
Remisa Yousefvand <remisa.yousefvand@gmail.com>
2020-08-27 16:24:45 +0430

    Code from plasmoid-persian-calendar :truck:

---
+++ tests/parser.test.js
@@ -0,0 +1,17 @@
+/* eslint-env jest */
+
+const data = require('./mock-data').formats
+const parser = require('../lib/parser')
+
+describe('parser', () => {
+  test('tokenize', () => {
+    data.forEach(d => {
+      expect(parser.tokenize(d.format)).toEqual(d.tokens)
+    })
+  })
+  // test('parse', () => {
+  //   data.forEach(d => {
+  //     expect(parser.parse(d.format, d.now)).toBe(d.display)
+  //   })
+  // })
+})
