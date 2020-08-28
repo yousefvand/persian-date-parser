598f922d455282d2673dea7346ba6ddfff1aa9d1
Remisa Yousefvand <remisa.yousefvand@gmail.com>
2020-08-28 14:28:32 +0430

    fixed unit test :eyes:

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,12 +1,12 @@
 /* eslint-env jest */
 
 const data = require('./mock-data').formats
-const PersianDateParser = require('../index')
+const pdp = require('../index')
 
 test('library', () => {
   data.forEach(d => {
-    expect(PersianDateParser.parse(d.format, d.now, true)).toBe(d.display)
-    expect(PersianDateParser.parse(d.format, d.now, false)).toBe(d.display)
+    expect(pdp(d.format, d.now, true)).toBe(d.display)
+    expect(pdp(d.format, d.now, false)).toBe(d.display)
     // console.log(PersianDateParser.parse('pjyyyy/pjmm/pjdd pjHH:pjMM pjTT', new Date()))
   })
 })
